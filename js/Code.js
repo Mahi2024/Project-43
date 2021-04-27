73222b67f123ccabf3dd632e0de099d427122366
Mahi2024 <77480600+Mahi2024@users.noreply.github.com>
2021-04-27 15:50:51 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white");
+    textSize(15);
+    text("V B L R E A I A",100,50);
+    fill("lightblue");
+    text("Hint: All ways changing, not constant",100,70);
+
+    fill("white");
+    textSize(15);
+    text("N U C F O T I N",700,150);
+    fill("lightblue");
+    text("Hint: Performs a specific task",700,170);
+
+    fill("white");
+    textSize(15);
+    text("E A B A T A D S",100,250);
+    fill("lightblue");
+    text("Hint: Stores information",100,270);
+}
